4511ce8b901c2f5eea6c65732748b1cd92962622
LJH <hosboy93@naver.com>
2023-07-12 15:25:35 +0900

    useState 이벤트

---
+++ hello-app/src/App6.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+
+import Say from './comp/Say';
+
+class App5 extends Component {
+  render() {
+    return (
+      <div className="react">
+        <Say />
+      </div>
+    );
+  }
+}
+
+export default App5;
