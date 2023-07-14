8b8229b050ad12466b40c07dd4d380231e365fcf
LJH <hosboy93@naver.com>
2023-07-14 14:56:06 +0900

    root.render 수정

--- hello-app/src/index.js
+++ hello-app/src/index.js
@@ -16,14 +16,17 @@ import App12 from './App12';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
 root.render(
   <React.StrictMode>
-    {/* <App10 /> */}
+    <App10 />
     <App12 />
   </React.StrictMode>
 );
+ */
+root.render(<App12 />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
