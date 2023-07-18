7c08b6d9ea73dedb20602f22850395d7eda9a00a
LJH <hosboy93@naver.com>
2023-07-18 11:24:59 +0900

    TodoList, TodoListItem

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
 import TodoTemplate from './TodoTemplate';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
