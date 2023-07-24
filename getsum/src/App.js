91a063f3ddf9e6d152505c043d495cf5d6855b08
LJH <hosboy93@naver.com>
2023-07-24 18:02:15 +0900

    과제

--- getsum/src/App.js
+++ getsum/src/App.js
@@ -7,17 +7,29 @@ function App() {
     status: -1,
     result: 0,
     message: '',
+    uuid: '',
   });
 
   const onSum = async (e) => {
     // alert('계산 요청');
-    const response = await axios.get('/ServerSum/sum.jsp').then();
-    setState(response.data);
+    const response = await axios.get('http://localhost:8090/api/createId');
+    console.log(response.data);
+    console.log(response.headers['sessionid']);
+    if (response.data.status === 1) {
+      setState({ ...state, uuid: response.data.uuid });
+      console.log(response.data.uuid);
+      const response2 = await axios.get(
+        'http://localhost:8090/api/sum?uuid=' + response.data.uuid
+      );
+      setState(response2.data);
+    }
   };
 
   const onSumCancel = async (e) => {
     // alert('계산 취소');
-    const response = await axios.get('/ServerSum/sumCancle.jsp').then();
+    const response = await axios.get(
+      'http://localhost:8090/api/sumCancle?uuid=' + state.uuid
+    );
     setState(response.data);
   };
   return (
@@ -25,6 +37,7 @@ function App() {
       <div>
         <h1>axios</h1>
         <p>{state.status === 1 ? state.result : ''}</p>
+        <p>{state.message}</p>
         <button onClick={onSum}>계산 요청</button>
         <button onClick={onSumCancel}>계산 취소</button>
       </div>
