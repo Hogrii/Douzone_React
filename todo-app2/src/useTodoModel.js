7dcc3cf463eb23412d307a63844c8e16b366caaf
LJH <hosboy93@naver.com>
2023-07-31 20:44:29 +0900

    주석제거

--- todo-app2/src/useTodoModel.js
+++ todo-app2/src/useTodoModel.js
@@ -12,25 +12,7 @@ const createBulkTodos = () => {
   }
   return array;
 };
-/* 
- // 여기는 지역변수(todos)
-setTodos((todos) =>
-    todos.concat({
-    id: nextId.current++,
-    checked: false,
-    title: value,
-    })
-);
-// myTodosAppend(react.todos) -> react가 가지고 있음
-*/
-// setTodos((todos) => todos.filter((todo) => todo.id !== id));
-/*
-setTodos((todos) =>
-    todos.map((todo) =>
-    todo.id === id ? { ...todo, checked: !todo.checked } : todo
-    )
-);
- */
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'insertTodo':
@@ -42,22 +24,6 @@ const reducer = (state, action) => {
         todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
       );
 
-    // 이진탐색
-    // let left = 0;
-    // let right = todos.length - 1;
-    // let targetIndex = -1;
-    // while (left <= right) {
-    //   let index = Math.floor((left + right) / 2);
-    //   if (todos[index].id === action.id) {
-    //     targetIndex = index;
-    //     break;
-    //   } else if (todos[index].id > action.id) {
-    //     right = index - 1;
-    //   } else left = index + 1;
-    // }
-    // todos[targetIndex].checked = !todos[targetIndex].checked;
-    // return [...todos];
-
     default:
       return state;
   }
@@ -65,23 +31,7 @@ const reducer = (state, action) => {
 
 const useTodoModel = () => {
   const [state, dispatch] = useReducer(reducer, createBulkTodos());
-  //   const [todos, setTodos] = useState(createBulkTodos);
   const nextId = useRef(state.length + 1);
-
-  /*
-    const insertTodo = useCallback(
-      (value) => {
-        setTodos(
-          todos.concat({
-            id: nextId.current++,
-            checked: false,
-            title: value,
-          })
-        );
-      },
-      [todos]
-    );
-     */
 
   const insertTodo = useCallback((value) => {
     dispatch({
@@ -103,18 +53,6 @@ const useTodoModel = () => {
     dispatch({ type: 'onToggle', id: id });
   }, []);
 
-  /*
-    // 옛날 방법
-    const onChangeChecked = useCallBack((id) => {
-      for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id === id) {
-          todos[i].checked = !todos[i].chcked;
-          setTodos([...todos]);
-          break;
-        }
-      }
-    }, todos);
-     */
   return {
     state,
     actions: {
